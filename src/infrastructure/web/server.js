051a34f265878eb1bdbfc79fa0e7507217c68c68
Serhii Moskovko <cakedispensers@gmail.com>
2025-03-09 21:17:17 +0200

    feat: modify server to use setup functions

--- src/infrastructure/web/server.js
+++ src/infrastructure/web/server.js
@@ -1,37 +1,26 @@
-import fastifyStatic from '@fastify/static';
-import fastifyView from '@fastify/view';
 import Fastify from 'fastify';
-import path from 'node:path';
-import nunjucks from 'nunjucks';
+import { setupPlugins } from './setup/plugins.js';
+import { setupRoutes } from './setup/routes.js';
+import { setupRepos } from './setup/repos.js';
+import { setupServices } from './setup/services.js';
+import { setupDI } from './setup/di.js';
 
 const start = async () => {
   const fastify = Fastify({
     logger: true,
   });
 
-  fastify.register(fastifyStatic, {
-    root: path.join(process.cwd(), 'dist/public'),
-    prefix: '/public/',
-  });
+  await setupDI(fastify);
 
-  fastify.register(fastifyView, {
-    engine: { nunjucks },
-    root: path.join(process.cwd(), 'dist/views'),
-  });
+  await setupPlugins(fastify);
 
-  fastify.get('/', async (request, reply) => {
-    return reply.view('pages/home.html', {
-      title: 'Home Page',
-    });
-  });
+  await setupServices(fastify);
 
-  fastify.get('/about', async (request, reply) => {
-    return reply.view('pages/about.html', {
-      title: 'About Page',
-    });
-  });
+  await setupRepos(fastify);
 
-  fastify.listen({ port: 3000, host: '0.0.0.0' }, function (err) {
+  await setupRoutes(fastify);
+
+  fastify.listen({ port: 3000, host: '0.0.0.0' }, (err) => {
     if (err) {
       fastify.log.error(err);
       process.exit(1);
