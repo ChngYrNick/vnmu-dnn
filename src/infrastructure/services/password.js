170946266d3af40bfdf7cf0fee9a982ab0045275
Serhii Moskovko <cakedispensers@gmail.com>
2025-03-13 08:46:17 +0200

    refactor: password service

--- src/infrastructure/services/password.js
+++ src/infrastructure/services/password.js
@@ -3,17 +3,19 @@ import { promisify } from 'util';
 
 const scryptAsync = promisify(scrypt);
 
-const hash = async (password) => {
-  const salt = randomBytes(16).toString('hex');
-  const buf = await scryptAsync(password, salt, 64);
-  return `${buf.toString('hex')}.${salt}`;
-};
+class PasswordService {
+  static async hash(password) {
+    const salt = randomBytes(16).toString('hex');
+    const buf = await scryptAsync(password, salt, 64);
+    return `${buf.toString('hex')}.${salt}`;
+  }
 
-const compare = async (storedPass, suppliedPass) => {
-  const [hashedPass, salt] = storedPass.split('.');
-  const hashedPassBuf = Buffer.from(hashedPass, 'hex');
-  const suppliedPassBuf = await scryptAsync(suppliedPass, salt, 64);
-  return timingSafeEqual(hashedPassBuf, suppliedPassBuf);
-};
+  static async compare(storedPass, suppliedPass) {
+    const [hashedPass, salt] = storedPass.split('.');
+    const hashedPassBuf = Buffer.from(hashedPass, 'hex');
+    const suppliedPassBuf = await scryptAsync(suppliedPass, salt, 64);
+    return timingSafeEqual(hashedPassBuf, suppliedPassBuf);
+  }
+}
 
-export { hash, compare };
+export { PasswordService };
