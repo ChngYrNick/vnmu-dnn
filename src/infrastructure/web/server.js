f1364bae6a3dbdeee1a5d200bc32209839570739
Serhii Moskovko <cakedispensers@gmail.com>
2025-03-14 17:33:40 +0200

    refactor: move setup fuctions to init

--- src/infrastructure/web/server.js
+++ src/infrastructure/web/server.js
@@ -1,28 +1,10 @@
 import Fastify from 'fastify';
-import { setupPlugins } from './setup/plugins.js';
-import { setupRoutes } from './setup/routes.js';
-import { setupRepos } from './setup/repos.js';
-import { setupServices } from './setup/services.js';
-import { setupDI } from './setup/di.js';
 import { init } from './setup/init.js';
-import { setupDB } from './setup/db.js';
 
 const start = async () => {
   const fastify = Fastify({ logger: true });
   const port = process.env.PORT || 3000;
   const host = process.env.HOST || '127.0.0.1';
-
-  await setupDI(fastify);
-
-  await setupDB(fastify);
-
-  await setupServices(fastify);
-
-  await setupRepos(fastify);
-
-  await setupPlugins(fastify);
-
-  await setupRoutes(fastify);
 
   await init(fastify);
 
--- src/infrastructure/web/setup/init.js
+++ src/infrastructure/web/setup/init.js
@@ -1,6 +1,27 @@
+import { setupDI } from './di.js';
+import { setupDB } from './db.js';
+import { setupServices } from './services.js';
+import { setupRepos } from './repos.js';
+import { setupPlugins } from './plugins.js';
+import { setupRoutes } from './routes.js';
+
 const init = async (fastify) => {
+  await setupDI(fastify);
+
+  await setupDB(fastify);
+
+  await setupServices(fastify);
+
+  await setupRepos(fastify);
+
+  await setupPlugins(fastify);
+
+  await setupRoutes(fastify);
+
   const { queryLoaderService, setupRepo } = fastify.di;
+
   await queryLoaderService.init();
+
   if (fastify.initDB) {
     await setupRepo.exec();
   }
