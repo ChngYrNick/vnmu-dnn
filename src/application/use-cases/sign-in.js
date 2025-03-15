8667a33cad7c100b5538b85e2da6c5a61bfb39ec
Serhii Moskovko <cakedispensers@gmail.com>
2025-03-15 13:34:34 +0200

    feat(use-cases): implement sign in

---
+++ src/application/use-cases/sign-in.js
@@ -0,0 +1,35 @@
+import { UnauthError } from 'src/domain/errors/unauth-error.js';
+
+class SignInUseCase {
+  #userRepo = null;
+  #passwordService = null;
+  #sessionsService = null;
+
+  constructor({ userRepo, passwordService, sessionsService }) {
+    this.#userRepo = userRepo;
+    this.#passwordService = passwordService;
+    this.#sessionsService = sessionsService;
+  }
+
+  async exec({ username, password, email }) {
+    if (await this.#sessionsService.checkAuth()) {
+      throw new UnauthError("Can't sign in while active session");
+    }
+
+    const user = this.#userRepo.readByInfo({ username, email });
+
+    if (!user) {
+      throw new UnauthError("The user with this credentials doesn't exists");
+    }
+
+    const valid = await this.#passwordService.compare(user.password, password);
+
+    if (!valid) {
+      throw new UnauthError('Provided password is not valid');
+    }
+
+    await this.#sessionsService.startSession(user);
+  }
+}
+
+export { SignInUseCase };
