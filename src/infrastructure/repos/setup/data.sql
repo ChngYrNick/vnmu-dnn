6757cecc40dc932a7d5a9cfdb979081c618bcafc
Serhii Moskovko <cakedispensers@gmail.com>
2025-03-15 12:04:50 +0200

    feat: add full name to users schema

--- src/infrastructure/repos/setup/data.sql
+++ src/infrastructure/repos/setup/data.sql
@@ -5,9 +5,10 @@ INSERT INTO roles (id, name) VALUES (2, 'admin');
 -- username: admin
 -- password: admin
 
-INSERT INTO users (username, email, password, roleId) VALUES (
+INSERT INTO users (username, email, fullName, password, roleId) VALUES (
   'admin',
   'temp@mail.com',
+  'Admin',
   '8d89b5e2772fa4161f3ba737b041f8e2156d1c7bfa11b13247566428fe24cec3a1a5a1cb94552310cf2ec3ef074d6082255efe03d09d0b469772045942dad47f.57c9341390db8345050e252b7eb4e0d3',
   2
 );
