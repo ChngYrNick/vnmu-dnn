95308b061d53d9996059e4689b85a49a1aa6f839
Serhii Moskovko <cakedispensers@gmail.com>
2025-03-20 08:52:08 +0200

    Revert "chore: update vite config to watch new added files in view and assets dirs"
    This reverts commit 17028d28ff684b1003614e12961b5e195b7ae5b2.

--- vite.config.js
+++ vite.config.js
@@ -1,68 +1,49 @@
-import path from 'node:path';
+import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 import { sync } from 'glob';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-const rootPath = path.resolve(process.cwd(), 'src/infrastructure/web/ui');
+const rootPath = resolve(process.cwd(), 'src/infrastructure/web/ui');
 
-export default defineConfig(({ command }) => {
-  return {
-    root: rootPath,
-    resolve: {
-      alias: {
-        '~bootstrap': path.resolve(process.cwd(), 'node_modules/bootstrap'),
+export default defineConfig({
+  root: rootPath,
+  resolve: {
+    alias: {
+      '~bootstrap': resolve(process.cwd(), 'node_modules/bootstrap'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        quietDeps: true,
       },
     },
-    css: {
-      preprocessorOptions: {
-        scss: {
-          quietDeps: true,
-        },
-      },
-    },
-    build: {
-      watch:
-        command === 'build' && process.argv.includes('--watch') ? {} : null,
-      outDir: path.resolve(process.cwd(), 'dist'),
-      rollupOptions: {
-        input: sync(path.resolve(rootPath, 'views', '**/*.html')),
-        output: {
-          assetFileNames: (assetInfo) => {
-            if (/\.(css)$/.test(assetInfo.names.at(0))) {
-              return 'public/styles/[name]-[hash].[ext]';
-            }
-            return 'public/assets/[name]-[hash].[ext]';
-          },
-          entryFileNames: 'public/scripts/[name]-[hash].js',
-          chunkFileNames: 'public/scripts/[name]-[hash].js',
-        },
-      },
-      minify: true,
-    },
-    plugins: [
-      viteStaticCopy({
-        targets: [
-          {
-            src: path.resolve(rootPath, 'assets', '*'),
-            dest: 'public/assets',
-          },
-        ],
-      }),
-      {
-        name: 'watch-external',
-        buildStart() {
-          this.addWatchFile(rootPath);
-        },
-        handleHotUpdate({ file, server }) {
-          if (
-            file.includes(path.join('views')) ||
-            file.includes(path.join('assets'))
-          ) {
-            server.restart();
-            return [];
+  },
+  build: {
+    outDir: resolve(process.cwd(), 'dist'),
+    rollupOptions: {
+      input: sync(resolve(rootPath, 'views', '**/*.html')),
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (/\.(css)$/.test(assetInfo.names.at(0))) {
+            return 'public/styles/[name]-[hash].[ext]';
           }
+          return 'public/assets/[name]-[hash].[ext]';
         },
+        entryFileNames: 'public/scripts/[name]-[hash].js',
+        chunkFileNames: 'public/scripts/[name]-[hash].js',
       },
-    ],
-  };
+    },
+    minify: true,
+  },
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: resolve(rootPath, 'assets', '*'),
+          dest: 'public/assets',
+        },
+      ],
+    }),
+  ],
 });
