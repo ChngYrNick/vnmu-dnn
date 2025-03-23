ceb4831e95e56e26c9bb774ec7c5486f7d764520
Serhii Moskovko <cakedispensers@gmail.com>
2025-03-23 17:42:13 +0200

    bugfix(sign-up): fix repeat password validation

--- src/infrastructure/web/ui/scripts/sign-up.js
+++ src/infrastructure/web/ui/scripts/sign-up.js
@@ -1,15 +1,18 @@
 import '../styles/main.scss';
 import '../styles/sign-up.scss';
 
-document.addEventListener('load', () => {
-  document.getElementById('repeatPassword').addEventListener('input', () => {
-    const password = document.getElementById('password').value;
-    const confirmPassword = this.value;
+addEventListener('load', () => {
+  const passwordElem = document.getElementById('password');
+  const repeatPasswordElem = document.getElementById('repeatPassword');
+
+  repeatPasswordElem.addEventListener('input', () => {
+    const password = passwordElem.value;
+    const confirmPassword = repeatPasswordElem.value;
 
     if (password !== confirmPassword) {
-      this.setCustomValidity('Passwords do not match');
+      repeatPasswordElem.setCustomValidity('Passwords do not match');
     } else {
-      this.setCustomValidity('');
+      repeatPasswordElem.setCustomValidity('');
     }
   });
 });
