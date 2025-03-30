4574cb95eb086fd2411ed5c0f931d3ca8ee0f354
Serhii Moskovko <cakedispensers@gmail.com>
2025-03-30 10:06:09 +0300

    feat(error-handler): use hx-redirect header for hx-request

--- src/infrastructure/web/setup/error.js
+++ src/infrastructure/web/setup/error.js
@@ -5,13 +5,17 @@ const setupErrorHandling = async (fastify) => {
     const result = req.di.errorService.handle(error);
     const params = new URLSearchParams(result);
     const url = `/error?${params.toString()}`;
-    return reply.redirect(url);
+    return req.headers['hx-request']
+      ? reply.header('HX-Redirect', url).status(200).send()
+      : reply.redirect(url);
   });
   fastify.setNotFoundHandler((req, reply) => {
     const result = req.di.errorService.handle(new NotFoundError());
     const params = new URLSearchParams(result);
     const url = `/error?${params.toString()}`;
-    return reply.redirect(url);
+    return req.headers['hx-request']
+      ? reply.header('HX-Redirect', url).status(200).send()
+      : reply.redirect(url);
   });
 };
 
