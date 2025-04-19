29fa4c7d8507ee1ad8d873f3756419577002ae4b
Serhii Moskovko <cakedispensers@gmail.com>
2025-04-19 22:51:04 +0300

    feat(pages-repo): add redy by id method

--- src/infrastructure/repos/pages/pages.js
+++ src/infrastructure/repos/pages/pages.js
@@ -11,6 +11,11 @@ class PagesRepo {
     const query = this.#loader.get('pages/select.sql');
     return this.#db.prepare(query).all();
   }
+
+  async readById(pageId) {
+    const query = this.#loader.get('pages/select-by-id.sql');
+    return this.#db.prepare(query).get(pageId);
+  }
 }
 
 export { PagesRepo };
