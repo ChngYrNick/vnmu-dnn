661b7d55bf3e4402b736d6348e07aaf56cf3305c
Serhii Moskovko <cakedispensers@gmail.com>
2025-04-24 15:57:59 +0300

    feat(editor): add default config

--- src/infrastructure/web/ui/scripts/editor.js
+++ src/infrastructure/web/ui/scripts/editor.js
@@ -2,14 +2,21 @@ import EasyMDE from 'easymde';
 
 class VNMUEditorComponent extends HTMLElement {
   static tagName = 'vnmu-editor';
+  static defaultConfig = {
+    showIcons: ['table'],
+    toolbarButtonClassPrefix: 'mde',
+  };
 
-  editor = null;
-  textAreaElement = null;
+  #editor = null;
+  #textAreaElement = null;
 
   connectedCallback() {
-    this.textAreaElement = document.createElement('textarea');
-    this.appendChild(this.textAreaElement);
-    this.editor = new EasyMDE({ element: this.textAreaElement });
+    this.#textAreaElement = document.createElement('textarea');
+    this.appendChild(this.#textAreaElement);
+    this.#editor = new EasyMDE({
+      ...VNMUEditorComponent.defaultConfig,
+      element: this.textAreaElement,
+    });
   }
 
   static from() {
