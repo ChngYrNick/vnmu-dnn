04f383bde06f0d1d29ed4a50adb1af16c1d88e23
Serhii Moskovko <cakedispensers@gmail.com>
2026-01-16 23:08:15 +0200

    feat(pages-home): implement quick links to student materials

--- src/application/use-cases/get-home-page-details.js
+++ src/application/use-cases/get-home-page-details.js
@@ -3,18 +3,33 @@ class GetHomePageDetailsUseCase {
   #newsRepo = null;
   #newsContentRepo = null;
   #newsFileRepo = null;
+  #specialtiesRepo = null;
+  #specialtiesContentRepo = null;
+  #studentMaterialsRepo = null;
 
-  constructor({ contactsRepo, newsRepo, newsContentRepo, newsFileRepo }) {
+  constructor({
+    contactsRepo,
+    newsRepo,
+    newsContentRepo,
+    newsFileRepo,
+    specialtiesRepo,
+    specialtiesContentRepo,
+    studentMaterialsRepo,
+  }) {
     this.#contactsRepo = contactsRepo;
     this.#newsRepo = newsRepo;
     this.#newsContentRepo = newsContentRepo;
     this.#newsFileRepo = newsFileRepo;
+    this.#specialtiesRepo = specialtiesRepo;
+    this.#specialtiesContentRepo = specialtiesContentRepo;
+    this.#studentMaterialsRepo = studentMaterialsRepo;
   }
 
   async exec({ language }) {
-    const [contacts, newsList] = await Promise.all([
+    const [contacts, newsList, quickLinks] = await Promise.all([
       this.#contactsRepo.read(),
       this.#newsRepo.findPublishedPaginated(3, 0),
+      this.#getSpecialtyQuickLinks(language),
     ]);
 
     const newsWithContent = await Promise.all(
@@ -40,7 +55,35 @@ class GetHomePageDetailsUseCase {
       }),
     );
 
-    return { contacts, news: newsWithContent };
+    return { contacts, news: newsWithContent, quickLinks };
+  }
+
+  async #getSpecialtyQuickLinks(language) {
+    const allSpecialties = await this.#specialtiesRepo.find();
+    const limitedSpecialties = allSpecialties.slice(0, 6);
+
+    const quickLinks = await Promise.all(
+      limitedSpecialties.map(async (specialty) => {
+        const [content, courses] = await Promise.all([
+          this.#specialtiesContentRepo.readByInfo({
+            specialtyId: specialty.id,
+            language,
+          }),
+          this.#studentMaterialsRepo.findBySpecialty(specialty.id),
+        ]);
+
+        // courses are ordered by course number (ascending)
+        const firstCourse = courses[0]?.course;
+        let url = `/student?specialty=${specialty.slug}`;
+        if (firstCourse) {
+          url += `&course=${firstCourse}`;
+        }
+
+        return { name: content?.name || specialty.slug, url };
+      }),
+    );
+
+    return quickLinks;
   }
 }
 
